05113b0ec42d8abd0dd1125cf126340ba9479866
Veysi yê MALA SAF <veysieken000@gmail.com>
2026-07-07 08:46:33 +0300

    feat(core): expose provider abstraction module

--- reference-implementation/core/src/lib.rs
+++ reference-implementation/core/src/lib.rs
@@ -1,3 +1,5 @@
+pub mod provider;
+
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -68,6 +70,5 @@ pub enum RuntimeState {
     Completed,
     Failed,
 }
-
 
 
