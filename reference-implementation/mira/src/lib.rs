40c6142758c84d984359627d6baf70b0a7132049
Veysi yê MALA SAF <veysieken000@gmail.com>
2026-07-25 13:40:45 +0300

    feat(mira): add deterministic PDF generator skeleton

--- reference-implementation/mira/src/lib.rs
+++ reference-implementation/mira/src/lib.rs
@@ -41,6 +41,7 @@ pub mod citation_reference_matcher;
 pub mod source_verification_report;
 pub mod bibtex_generator;
 pub mod latex_generator;
+pub mod pdf_generator;
 
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
