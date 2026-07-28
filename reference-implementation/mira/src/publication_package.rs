4b077ec3f079cbd03c8a8d6b69be2ad87f575650
Veysi yê MALA SAF <veysieken000@gmail.com>
2026-07-28 06:51:44 +0300

    Fix formatting issues in publication_package.rs

--- reference-implementation/mira/src/publication_package.rs
+++ reference-implementation/mira/src/publication_package.rs
@@ -67,11 +67,12 @@ impl PublicationRequest {
             package,
         }
     }
-   }
 
     pub fn is_ready(&self) -> bool {
         self.package.is_ready_for_publication()
     }
+}
+
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PublicationResult {
